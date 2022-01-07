882f4a4e97e474a616edba0cfb28ec7831de1720
c-indu <96856541+c-indu@users.noreply.github.com>
2022-01-07 16:38:34 +0530

    Exercise2

---
+++ Victory2.java
@@ -0,0 +1,21 @@
+package exercise;
+
+public class Victory2 {
+ public static void main(String[] args) {
+            System.out.println("/////////////////////");
+            victory();
+            victory();
+            victory();
+            victory();
+            victory();
+    
+        }
+        public static void victory(){
+            System.out.println("|| Victory is mine! ||");
+            System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        } 
+    
+    
+}
+    
+
